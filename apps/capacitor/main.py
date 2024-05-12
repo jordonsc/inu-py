bc90e0012a7838b12c1a5ee54cdb06b456932461
Jordon Scott <jordonsc@gmail.com>
2024-05-12 15:14:28 +1000

    Added in remaining config for cap sensors

--- apps/capacitor/main.py
+++ apps/capacitor/main.py
@@ -2,19 +2,33 @@ import asyncio
 import time
 
 import machine
+from inu import const
 
 from inu.app import InuApp
 from inu.schema.settings.sensors import Capacitor as CapacitorSettings
 
 
 class CapacitorApp(InuApp):
-    POLL_RATE = 0.5
+    # Sensor polling rate in seconds
+    POLL_RATE = 0.05
+    # Percentile delta to trigger a change report
+    REPORT_CHANGE = 1
 
     def __init__(self):
         super().__init__(CapacitorSettings)
         self.sensor = machine.TouchPad(machine.Pin(self.get_config(["sensor", "pin"], 8), machine.Pin.IN))
         self.last_poll = time.time()
         self.active = False
+        self.new_state = False
+
+        # Time we've been in a new state (for sensitivity delays)
+        self.state_change_time = 0
+
+        # For updating the device status - value we last reported
+        self.last_reported_value = 0
+
+        # For refire delay - time since we last sent a trigger
+        self.active_since = 0
 
     async def app_init(self):
         await self.set_state_from_last()
@@ -26,8 +40,13 @@ class CapacitorApp(InuApp):
         # Poll the capacitive sensor
         self.last_poll = time.time()
         raw_value = self.sensor.read()
-        value = round(self.map_value(raw_value, self.inu.settings.sensor_low, self.inu.settings.sensor_high, 0, 100), 2)
-        self.logger.info(f"Sensor value: {raw_value} -> {value}%")
+        value = self.map_value(raw_value, self.inu.settings.sensor_low, self.inu.settings.sensor_high, 0, 100)
+
+        # Report if the value has changed by at least REPORT_CHANGE percent
+        if self.last_reported_value == 0:
+            report_state = True
+        else:
+            report_state = abs(value - self.last_reported_value) > self.REPORT_CHANGE
 
         # Work out in which orientation we activate or deactivate the trigger
         if self.inu.settings.trigger_on < self.inu.settings.trigger_off:
@@ -37,19 +56,45 @@ class CapacitorApp(InuApp):
             do_activate = value > self.inu.settings.trigger_on
             do_deactivate = value < self.inu.settings.trigger_off
 
-        # Fire status updates
+        # Delay-wait before activating or deactivating
         if do_activate and not self.active:
-            self.active = True
-            await self.on_trigger(self.inu.settings.trigger_code)
-            await self.inu.status(active=True, status=f"{value}%")
+            if self.new_state:
+                if time.time() - self.state_change_time >= (self.inu.settings.delay_wait / 1000):
+                    self.active = True
+                    report_state = True
+                    await self.fire()
+            else:
+                self.new_state = True
+                self.state_change_time = time.time()
 
         elif do_deactivate and self.active:
-            self.active = False
-            await self.on_trigger(self.inu.settings.trigger_code)
-            await self.inu.status(active=False, status='')
+            if not self.new_state:
+                if time.time() - self.state_change_time >= (self.inu.settings.delay_wait / 1000):
+                    self.active = False
+                    report_state = True
+            else:
+                self.new_state = False
+                self.state_change_time = time.time()
 
-        elif self.active:
-            await self.inu.status(status=f"{value}%")
+        # Update device state
+        if report_state:
+            await self.inu.status(active=self.active, status=f"{value:.1f}% ({raw_value})")
+            self.logger.debug(f"Sensor value: {value:.1f}% ({raw_value})")
+            self.last_reported_value = value
+
+        # Trigger a refire
+        can_refire = (self.inu.settings.refire_delay > 0) and self.active
+        if can_refire and time.time() - self.active_since > (self.inu.settings.refire_delay / 1000):
+            await self.fire()
+
+    async def fire(self):
+        """
+        Dispatch a trigger and update the time that we last triggered.
+        """
+        self.active_since = time.time()
+        await self.inu.command(const.Subjects.COMMAND_TRIGGER, {
+            'code': self.inu.settings.trigger_code,
+        })
 
 
 if __name__ == "__main__":
